25a93b6281bd7687c6ac77a4f3aa46c8173836c3
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-26 09:43:37 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,35 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if  earthquake: 
+        body = {'id': earthquake.id,
+                'location': earthquake.location, 
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year}  
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}    
+        status = 404 
+     
+    return make_response(body, status)     
+    
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes_data = [
+        {
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        }
+        for quake in quakes
+    ]
+    return jsonify({'count': len(quakes_data), 'quakes': quakes_data}), 200
 
 
 if __name__ == '__main__':
